be7c80472f66fab3b940dc2ec5999989318a33da
Rahul kumar <rahulgupta707062@gmail.com>
2026-08-15 22:37:06 +0530

    Initial Career Circle setup

---
+++ Array/Q02_SmallestElement.java
@@ -0,0 +1,18 @@
+package Array;
+
+public class Q02_SmallestElement {
+     public static void main(String[] args) {
+
+        int[] arr = {10, 25, 5, 40, 15};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Smallest: " + min);
+    }
+}
