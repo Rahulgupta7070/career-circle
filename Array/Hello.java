d646026782bb5caaef853c683b338717b636937e
ManishKr2026 <manishparker15022003@gmail.com>
2026-08-16 01:28:19 +0530

    i am adding

--- Array/Hello.java
+++ Array/Hello.java
@@ -3,6 +3,6 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Java");
-        System.out.println("Developer");
+        System.out.println("Hi");
     }
 }
