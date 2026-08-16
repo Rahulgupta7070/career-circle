cb31d5a2178b2b21365eff3a5b971d658e07f89b
ManishKr2026 <manishparker15022003@gmail.com>
2026-08-16 21:47:37 +0530

    Added coding questions

--- String/reverseString.java
+++ String/reverseString.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class reverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a String: ");
+        System.out.print("Enter a String: ");
         String str = sc.nextLine();
         String rev = "";
         for(int i=str.length()-1; i>=0; i--) {
