88391a1a8759af6ebc9b1f005a5359faa516d325
Md Imran <imranazam115@gmail.com>
2026-08-16 12:55:56 +0530

    Adding string program

---
+++ String_Programs/stringLength.java
@@ -0,0 +1,10 @@
+package String_Programs;
+
+public class stringLength {
+    public class Main {
+    public static void main(String[] args) {
+        String name = "Imran";
+        System.out.println("Length: " + name.length());
+    }
+}
+}
