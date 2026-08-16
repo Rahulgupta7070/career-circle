40c6b41f631f3d7869548893e206cfcf16cb0cf6
sumit_raj <sraj66016@gmail.com>
2026-08-16 12:47:14 +0530

    This is hello world program

---
+++ Array/P2.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class P2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+}
--- TASKS/Task-01.md
+++
@@ -1,7 +0,0 @@
-Task 01: Find the Largest Element in an Array
-
-Input:
-10 25 5 40 15
-
-Output:
-40
