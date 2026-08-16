daeff716ce4197d228c2074e5a8bdb27fed12ee3
ManishKr2026 <manishparker15022003@gmail.com>
2026-08-16 13:10:08 +0530

    i am adding a reverse of string program

---
+++ String/reverseString.java
@@ -0,0 +1,18 @@
+package String;
+
+import java.util.Scanner;
+
+public class reverseString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String str = sc.nextLine();
+        String rev = "";
+        for(int i=str.length()-1; i>=0; i--) {
+            rev = rev+str.charAt(i);
+        }
+        System.out.println("Reverse : " + rev);
+    }
+    
+    
+}
