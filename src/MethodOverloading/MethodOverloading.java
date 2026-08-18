197444005cfbcab3ef21afc0f488d93d72fc4b84
Adii995 <adityakumarak9956@gmail.com>
2026-08-18 15:17:17 +0530

    add OverLoading and String,NumberProgram Question

---
+++ src/MethodOverloading/MethodOverloading.java
@@ -0,0 +1,22 @@
+package Concepts;
+
+public class MethodOverloading {
+	
+	public static void main(String[] args) {
+		System.out.println(add(10,40));
+		System.out.println(add(34.22,432.13));
+		System.out.println(add(10,50,80));
+	}
+	
+	
+  public static int add(int a,int b) {
+	  return a+b;
+  }
+  public static double add(double a,double b) {
+	  return a+b;
+  }
+  public static int add(int a,int b,int c) {
+	  return a+b+c;
+  }
+  
+}
---
+++ src/MethodOverriding/EmployeeMethoOverride.java
@@ -0,0 +1,7 @@
+package MethodOverriding;
+
+public class EmployeeMethoOverride {
+    public void work() {
+    	System.out.println("Employee is working");
+    }
+}
