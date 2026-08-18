63a9c895dd307882abe838ebeea79c2d0faac11e
Shivam <shivamkumar56421@gmail.com>
2026-08-18 19:31:46 +0530

    Updated Day 01 code

--- My_Program/Program.java
+++
@@ -1,7 +0,0 @@
-package My_Program;
-
-public class Program {
-    public static void main(String[] args) {
-        System.out.println("Hii this is my first code..!!");
-    }
-}
--- TASKS/Task-02.md
+++
@@ -1,7 +0,0 @@
-Task 02: Find the Smallest Element in an Array
-
-Input:
-10 25 5 40 15
-
-Output:
-5
