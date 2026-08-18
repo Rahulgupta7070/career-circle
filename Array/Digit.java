721240c69dad028e501d49fda6e0f17bc2beb84a
MOHD_KAMRUDDEEN <mohdkamruddeen42@gmail.com>
2026-08-18 13:39:13 +0530

    Add a Two Number

---
+++ Array/Digit.java
@@ -0,0 +1,18 @@
+package Array;
+import java.util.Scanner;
+
+public class Digit {
+    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter The First Number..");
+        int a=sc.nextInt();
+        System.out.println("Enter The Seconds Number...");
+        int b=sc.nextInt();
+
+        int c=a+b;
+        System.out.println("The Final Result is:"+c);
+        
+
+    }
+}
