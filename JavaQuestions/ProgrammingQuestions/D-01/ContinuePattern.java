1877d49f553a51bdb428fd90e3e9004970d29617
ManishKr2026 <manishparker15022003@gmail.com>
2026-08-19 21:16:22 +0530

    added Day-1 interview based programs

---
+++ JavaQuestions/ProgrammingQuestions/D-01/ContinuePattern.java
@@ -0,0 +1,11 @@
+class ContinuePattern {
+    public static void main(String[] args) {
+        int n = 1;
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=5; j++)
+                System.out.printf("%-4d", n++);
+            System.out.println();
+            
+        }
+    }
+}
---
+++ JavaQuestions/ProgrammingQuestions/D-01/Swap.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the number b : ");
+        int b = sc.nextInt();
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println(" b = " + b);
+    }
+}
