bba221b47fb03c0d6e9e593ff09ebd939c671a20
Shivam <shivamkumar56421@gmail.com>
2026-08-25 21:19:31 +0530

    Find second largest

---
+++ Assignments/FindSecondLargest.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class FindSecondLargest {
+    public static void main(String[] args) {
+        int arr[] = { 10, 5, 20, 8, 15 };
+        int largest = arr[0];
+        int largest_2 = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest_2 = largest;
+                largest = arr[i];
+
+            }
+        }
+        System.out.println(largest_2);
+    }
+}
