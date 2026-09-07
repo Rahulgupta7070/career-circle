6991e22734b6aedef4f5bf1793ebbf434e5012e0
Shivam <shivamkumar56421@gmail.com>
2026-09-07 13:45:34 +0530

    Count Strings Starting with 'A

---
+++ Day_10/CountString.java
@@ -0,0 +1,17 @@
+package Day_10;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CountString {
+    //
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Apple","Banana","Cherry","Avocado");
+        long count = names.stream()
+                          .filter(x -> x.startsWith("A"))
+                          .count();
+
+                          System.out.println(count);
+        
+    }
+}
